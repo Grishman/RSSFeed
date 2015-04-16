9d7499daa602ac07f0d39f809adc0be7178bbc3d
Andrew Grishman <grishman4life@gmail.com>
2015-04-16 21:05:04 +0300

    Add notifications

--- RSSFeed/app/src/main/java/com/grishman/rssfeed/sync/RSSFeedSyncAdapter.java
+++ RSSFeed/app/src/main/java/com/grishman/rssfeed/sync/RSSFeedSyncAdapter.java
@@ -2,17 +2,23 @@ package com.grishman.rssfeed.sync;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SyncRequest;
 import android.content.SyncResult;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
+import com.grishman.rssfeed.MainActivity;
 import com.grishman.rssfeed.R;
 import com.grishman.rssfeed.data.RSSFeedContract;
 
@@ -37,6 +43,9 @@ public class RSSFeedSyncAdapter extends AbstractThreadedSyncAdapter {
     // 60 seconds (1 minute) * 180 = 3 hours
     public static final int SYNC_INTERVAL = 60 * 180;
     public static final int SYNC_FLEXTIME = SYNC_INTERVAL / 3;
+    private static final long DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
+    private static final int FEED_NOTIFICATION_ID = 2801;
+    private int numMessages = 0;
 
     public RSSFeedSyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
@@ -83,7 +92,8 @@ public class RSSFeedSyncAdapter extends AbstractThreadedSyncAdapter {
             cv.put("pub_date", item.getPubDate());
             getContext().getContentResolver().insert(RSSFeedContract.FeedsEntry.CONTENT_URI, cv);
         }
-
+        // Notify user about new feeds
+        notifyNewFeeds();
     }
 
     /**
@@ -152,10 +162,11 @@ public class RSSFeedSyncAdapter extends AbstractThreadedSyncAdapter {
              * then call ContentResolver.setIsSyncable(account, AUTHORITY, 1)
              * here.
              */
-
+            onAccountCreated(newAccount, context);
         }
         return newAccount;
     }
+
     private static void onAccountCreated(Account newAccount, Context context) {
         /*
          * Since we've created an account
@@ -176,4 +187,41 @@ public class RSSFeedSyncAdapter extends AbstractThreadedSyncAdapter {
     public static void initializeSyncAdapter(Context context) {
         getSyncAccount(context);
     }
+
+    public void notifyNewFeeds() {
+        Context context = getContext();
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context);
+
+        mBuilder.setContentTitle("Get your fresh RSS!");
+        mBuilder.setContentText("Service complete update feed.");
+        mBuilder.setTicker("RSS feed is up to date");
+        mBuilder.setSmallIcon(R.drawable.ic_noti_rss);
+        mBuilder.setAutoCancel(true);
+
+      /* Increase notification number every time a new notification arrives */
+        mBuilder.setNumber(++numMessages);
+
+      /* Creates an explicit intent for an Activity in your app */
+        Intent resultIntent = new Intent(context, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+
+      /* Adds the Intent that starts the Activity to the top of the stack */
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+      /* notificationID allows you to update the notification later on. */
+        mNotificationManager.notify(FEED_NOTIFICATION_ID, mBuilder.build());
+    }
+
 }
