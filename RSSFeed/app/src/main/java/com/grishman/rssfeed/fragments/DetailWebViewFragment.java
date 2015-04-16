9f03eabc749c55277cb674c074496b8f0c073e98
Andrew Grishman <grishman4life@gmail.com>
2015-04-16 23:05:39 +0300

    Update webView to use DOM Try to restore state of webView

--- RSSFeed/app/src/main/java/com/grishman/rssfeed/fragments/DetailWebViewFragment.java
+++ RSSFeed/app/src/main/java/com/grishman/rssfeed/fragments/DetailWebViewFragment.java
@@ -19,25 +19,52 @@ public class DetailWebViewFragment extends Fragment {
 
     public static final String DETAIL_URL = "URL";
     private String mUrl;
+    private WebView detailWebView;
+    private Bundle webViewBundle;
 
     public DetailWebViewFragment() {
     }
 
     @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        detailWebView.saveState(outState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        detailWebView.restoreState(savedInstanceState);
+    }
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-        WebView myWebView = (WebView) rootView.findViewById(R.id.webview);
+        detailWebView = (WebView) rootView.findViewById(R.id.webview);
         Bundle arguments = getArguments();
         if (arguments != null) {
             mUrl = arguments.getString(DetailWebViewFragment.DETAIL_URL);
         }
         Log.d("URT TO DETAIL", "" + mUrl);
         // TODO Fix rotation
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.setWebViewClient(new FeedWebViewClient());
-        myWebView.loadUrl(mUrl);
+        detailWebView.getSettings().setJavaScriptEnabled(true);
+        detailWebView.getSettings().setDomStorageEnabled(true);
+        detailWebView.setWebViewClient(new FeedWebViewClient());
+//        detailWebView.loadUrl(mUrl);
+
+        if (webViewBundle == null) {
+            detailWebView.loadUrl(mUrl);
+        } else {
+            detailWebView.restoreState(webViewBundle);
+        }
         return rootView;
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        webViewBundle = new Bundle();
+        detailWebView.saveState(webViewBundle);
     }
 
     private class FeedWebViewClient extends WebViewClient {
